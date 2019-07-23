12a0658c8ef70ef363b8d1dbb1ec1cc1c6ebc6a9
Esteve Genovard Ferriol <isestevegf@gmail.com>
2019-07-24 00:54:33 +0200

    API Call

--- pages/education.js
+++ pages/education.js
@@ -1,16 +1,24 @@
 import React from "react";
 import Head from 'next/head'
+import fetch from 'isomorphic-unfetch';
 import Layout from "../components/Layout";
 
-const Education = () => (
+const Education = ({ stars }) => (
     <Layout>
         <Head>
             <title>Education</title>
         </Head>
         <div>
             <h1>Hello Education Next.js</h1>
+            <p>Next.js have { stars } stars on github</p>
         </div>
     </Layout>
 );
+
+Education.getInitialProps = async ({ req }) => {
+    const res = await fetch('https://api.github.com/repos/zeit/next.js');
+    const json = await res.json();
+    return { stars: json.stargazers_count };
+};
 
 export default Education;
